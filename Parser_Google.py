a24f46b9ee0970d64ace7d5ddf113369fdbd8cbd
AlexK <korshakov_07@mail.ru>
2020-04-12 01:22:50 +0300

    v9 optimized( + multiprocessing + log + timeran)

--- Parser_Google.py
+++ Parser_Google.py
@@ -1,36 +1,37 @@
+import concurrent.futures
 import os
 import random
 import re
 import time
+from functools import wraps
 import requests
-from win32com.client import Dispatch
 import win32com.client as com_client
 from bs4 import BeautifulSoup
 from requests import Response
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
 from tqdm import tqdm
-from functools import wraps
-from multiprocessing import Pool, freeze_support
+from win32com.client import Dispatch
 
-VIS_LOG = False  # Отображение хода процесса в консоли
-PRINT_LOG = True  # Запись лога в файл
+VIS_LOG = False  # True -  Отображение хода процесса в консоли
+PRINT_LOG = False  # True -  Запись лога в файл
 
-agents = ['Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)',
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)',
-          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)',
-          'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)',
-          'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0']
+agents = ['Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0']
+#           'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)',
+#           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)',
+#           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)',
+#           'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)']
 
 HEADERS = {'Accept': '*/*',
            'Connection': 'keep-alive',
            'Upgrade-Insecure-Requests': '1',
            'Cache-Control': 'max-age=0',
+           'host': 'www.google.com',
            'User-Agent': random.choice(agents)}
 
 
 def timeit(method):
-    """Деоратор отображения работы функции"""
+    """Деоратор отображения времени работы функции"""
 
     @wraps(method)
     def timed(*args, **kw):
@@ -39,20 +40,8 @@ def timeit(method):
         ms = (time.monotonic() - ts) * 1000
         sec: float = round(ms / 1000, 2)
 
-        print_args = False
-        print_kwgs = True
-
-        if print_args:
-            all_args = ', '.join(tuple(f'{a!r}' for a in args))
-            print('Время выполнения функции ' + f'{method.__name__}({all_args}): {ms:2.2f} ms или ' + str(sec) + 'сек.')
-        elif print_kwgs:
-            all_args = ', '.join(tuple(f'{k}={v!r}' for k, v in kw.items()))
-            print('Время выполнения функции ' + f'{method.__name__}({all_args}): {ms:2.2f} ms или ' + str(sec) + 'сек.')
-        elif print_args and print_kwgs:
-            all_args = ', '.join(tuple(f'{a!r}' for a in args) + tuple(f'{k}={v!r}' for k, v in kw.items()))
-            print('Время выполнения функции ' + f'{method.__name__}({all_args}): {ms:2.2f} ms или ' + str(sec) + 'сек.')
-        else:
-            print('Время выполнения функции ' + f'{method.__name__}: {ms:2.2f} ms или ' + str(sec) + 'сек.')
+        all_args = ', '.join(tuple(f'{k}={v!r}' for k, v in kw.items()))
+        print('Время выполнения функции ' + f'{method.__name__}({all_args}): {ms:2.2f} ms или ' + str(sec) + 'сек.')
         return result
 
     return timed
@@ -100,16 +89,16 @@ def log_visrec(param_name: str = None, param_value=None, r_log: bool = VIS_LOG, 
                 print(f'Параметр {param_name} Значение: {param_value}')
             else:
                 print(f'Параметр {param_name} Значение: {param_value[:200] + "..."}')
-    except:
-        print('log_visrec: Ошибка вывода в консоль')
+    except ConnectionError as ce:
+        log_visrec('log_visrec: Ошибка вывода в консоль', str(ce.args), True)
     try:
         if r_print:
             file = open('Log.txt', 'a', encoding='utf-8')
             text = f'Параметр {param_name}. Значение : {param_value}'
             file.write(text + '\n')
             file.close()
-    except:
-        print('log_visrec: Ошибка записи в файл')
+    except ConnectionError as ce:
+        log_visrec('log_visrec: Ошибка записи в файл', str(ce.args), True)
 
 
 def time_rand(t_start: int = 1, t_stop: int = 30):
@@ -131,7 +120,6 @@ def url_constructor(queries_path, selected_base_url, selected_region, max_pos=3)
 
     for ques in query:  # перебираем ключи и формируем url на их основе
         divs_ques: str = ques
-        # breakpoint()
         mod_url = selected_base_url + '?q=' + ques.replace(' ', '+') + selected_region + '&oq=' + ques.replace(
             ' ', '+') + selected_region + "&lr=lang_ru"
         log_visrec('url', mod_url)
@@ -148,92 +136,74 @@ def url_constructor(queries_path, selected_base_url, selected_region, max_pos=3)
 
 
 @timeit
-def Parser_YD_GOO(urls, divs_requests):
+def Parser_GOO(urls):
     """основная функция парсера"""
-    # создаём сессию (session), отправляем запрос (request), получаем ответ (request.text), варим суп (soup)
     url_counter: int = 1
-
-    adapter_yd = HTTPAdapter(max_retries=3)  # транспортный адаптер — максимальное количество повторов запроса
-    session = requests.Session()  # устанавливаем сессию
+    divs_requests_all: list = []  # создаем список  с ответами
 
     for it_url in urls:
         if url_counter != 1:  # Для всех ссылок КРОМЕ ПЕРВОЙ задаём рандомный промежуток задежки.
-            time_rand(2, 10)
-
-        session.mount(it_url['url'], adapter_yd)
-        # proxy?
+            time_rand(1, 4)
         try:
-            request: Response = session.get(it_url['url'], headers=HEADERS, stream=True, timeout=5.24)  # запрос
+            result = get_it(it_url['url'], it_url['ques'], url_counter)
+            divs_requests_all.extend(list(result))
             url_counter += 1
-            if request.status_code == 200:  # если запрос был выполнен успешно то
-                log_visrec('Parser_YD_GOO', 'Успешный запрос!')
-                # обработка ответа сервера
-                divs_requests = soup_request(request.text, url_counter, it_url['ques'], divs_requests)
-            else:
-                log_visrec('Parser_YD_GOO', 'Неудачный запрос! Ответ сервера{0}'.format(str(request.status_code)), True)
         except ConnectionError as ce:
-            log_visrec('Parser_YD_GOO', f'Ошибка при установке соединения! проверьте подключение!', True)
             log_visrec('Parser_YD_GOO', str(ce.args), True)
             continue
 
-    return divs_requests
+    return divs_requests_all
 
 
 @timeit
-def Parser_YD_GOO_parallel(urls, divs_requests, options=None, callback=None):
+def Parser_GOO_parallel(urls, max_process):
     """основная функция мульти парсера"""
-
-    pool_urls: list = []  # создаем список url
+    pool_urls: list = []  # создаем список / очередь url
     for key_urls in urls:
-        pool_urls.append(key_urls['url'])
-    log_visrec('check_headers_parallel_pool_urls', str(pool_urls))
+        pool_urls.append(key_urls['url'])  # создаем список / очередь url
 
-    # if not options:
-    # options= options.result()
+    divs_requests_all: list = []  # создаем список  с ответами
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_process)as executor:  # создаем очередь процессов
+        results = [executor.submit(get_it, my_url) for my_url in pool_urls]  # каждый процесс берёт свой URL
 
-    if Pool:
-        results = []
-        freeze_support()
-        pool = Pool(processes=4)
-        for url in pool_urls:
-            result = pool.apply_async(get_it, args=(url, divs_requests,), callback=callback)
-            time.sleep(0.5)
-            results.append(result)
-            log_visrec('check_headers_parallel', str(results))
+        for future in concurrent.futures.as_completed(results):  # Ответы
+            try:
+                if not len(list(future.result())) == 0:  # если результат что то содержит то добавляем
+                    divs_requests_all.extend(list(future.result()))  # расширяем лист
+            except Exception as ce:
+                log_visrec('my_company_cid', str(ce.args), True)
+        log_visrec('Parser_YD_GOO_parallel(divs_requests_all))', str(divs_requests_all))
 
-        pool.close()
-        pool.join()
-    else:
-        raise Exception('no parallelism supported')
-
-    log_visrec('Parser_YD_GOO_parallel_divs_requests', divs_requests)
-    return divs_requests
+    return divs_requests_all
 
 
 @timeit
-def get_it(url, divs_requests, ques=None):
-    """ функция посылает запрос и получает ответ"""
-    adapter_yd = HTTPAdapter(max_retries=3)  # транспортный адаптер — максимальное количество повторов запроса
+def get_it(url, ques=None, url_counter=None):
+    """ функция посылает запрос и получает ответ. Если ответ есть - передаёт на обработку"""
+    divs_requests: list = []  # создаем список c ответами
+    adapter_yd = HTTPAdapter(max_retries=3)  # транспортный адаптер — максимальное количество повторов запроса в сессии
     session = requests.Session()  # устанавливаем сессию
     session.mount(str(url), adapter_yd)
 
     try:
-        request: Response = session.get(url, headers=HEADERS, stream=True, timeout=10.24)  # запрос
+        request: Response = session.get(url, headers=HEADERS, stream=True, timeout=8.24)  # запрос
         if request.status_code == 200:  # если запрос был выполнен успешно то
             log_visrec('get_it', 'Успешный запрос!')
-            divs_requests = soup_request(request.text, 1, ques, divs_requests)  # обработка ответа сервера
-
+            divs_requests = soup_request(request.text, url_counter, ques, divs_requests)  # обработка ответа сервера
+            log_visrec('get_it_out(divs_requests)', str(divs_requests))
+        else:
+            log_visrec('Parser_YD_GOO', 'Неудачный запрос! Ответ сервера{0}'.format(str(request.status_code)), True)
     except Exception as ce:
         log_visrec('get_it', f'Ошибка при установке соединения! проверьте подключение!', True)
         log_visrec('get_it', str(ce.args), True)
 
-    log_visrec('get_it_divs_requests', divs_requests)
     return divs_requests
 
 
 @timeit
-def soup_request(r_text, url_counter, it_url_soup, divs_requests):
-    """ обработка ответа с помощью BeautifulSoup """
+def soup_request(r_text, url_counter, it_url_soup, divs_requests_soup):
+    """ обработка ответа с помощью BeautifulSoup. Если есть нужные данные - передаёт на поиск нужных данных в
+    divs_text_shelves """
     soup = BeautifulSoup(r_text, 'lxml')  # ответ
     divs = soup.find_all('li', class_='ads-ad')  # данные ответа
 
@@ -241,26 +211,24 @@ def soup_request(r_text, url_counter, it_url_soup, divs_requests):
         log_visrec('soup_request', f'Всего найдено блоков ' + str(len(divs)), True)
         # дербаним выдачу. передаём ответ сервера (divs), номер запроса (url_counter), ключ(it_url['ques']),
         # и словарь с распарсенной выдачей(divs_requests)
-        divs_text_shelves(divs, url_counter, it_url_soup, divs_requests)  # парсим данные ответа
+        divs_text_shelves(divs, url_counter, it_url_soup, divs_requests_soup)  # парсим данные ответа
         url_counter += 1
+        return divs_requests_soup
     else:
         log_visrec('soup_request', 'Ответ не содержит нужных данных :(', True)
-
-    log_visrec('soup_request_divs_requests', divs_requests)
-    return divs_requests
+    log_visrec('soup_request_out', divs_requests_soup, False, True)
 
 
 @timeit
 def divs_text_shelves(my_divs, url_counter, url_ques, my_divs_requests=None):
-    """парсим нужные данные ответа"""
-
+    """ищем нужные данные ответа"""
     if len(my_divs_requests) == 0:  # если лист со словарями(значениями) не создан - создаём сразу с заголовками
         my_divs_requests.append({'rowNom': 'п\п',  # i_row
                                  'ques': 'Ключ',  # url_ques
                                  'company_title': 'Название компании',  # my_company_title
                                  'company_cid': 'Позиция',  # my_company_cid
                                  'company_link_1': 'Ссылка',  # my_company_link_1
-                                 'company_sitelinks': 'Быстрая',  # my_company_sitelinks
+                                 'company_site_links': 'Быстрая',  # my_company_site_links
                                  'company_text': 'Описалово',  # my_company_text
                                  'company_contact': 'Контакты'})  # my_company_contact
     i_row: int = 1
@@ -268,24 +236,28 @@ def divs_text_shelves(my_divs, url_counter, url_ques, my_divs_requests=None):
         try:
             my_company_cid: str = str(i_row - 1)
             log_visrec('company_cid ', my_company_cid)
-        except:
+        except Exception as ce:
+            log_visrec('my_company_cid', str(ce.args), True)
             my_company_cid: str = ''
         try:
             my_company_link_1: str = DIV.find('div', attrs={'class': 'ads-visurl'}).text.replace('Реклама', ' ')
             x: int = my_company_link_1.index('/')
             my_company_link_1 = my_company_link_1[2:x]
             log_visrec('company_link_1 ', my_company_link_1)
-        except:
+        except Exception as ce:
+            log_visrec('my_company_link_1', str(ce.args), True)
             my_company_link_1: str = ''
         try:
-            my_company_sitelinks: str = DIV.find('div', attrs={'ul': 'OkkX2d'}).text
-            log_visrec('company_sitelinks ', my_company_sitelinks)
-        except:
-            my_company_sitelinks: str = ''
+            my_company_site_links: str = DIV.find('div', attrs={'ul': 'OkkX2d'}).text
+            log_visrec('company_site_links ', my_company_site_links)
+        except Exception as ce:
+            log_visrec('my_company_site_links', str(ce.args), True)
+            my_company_site_links: str = ''
         try:
             my_company_text: str = DIV.find('div', attrs={'class': 'ads-creative'}).text
             log_visrec('company_text ', my_company_text)
-        except:
+        except Exception as ce:
+            log_visrec('my_company_text', str(ce.args), True)
             my_company_text: str = ''
         try:
             my_company_contact: str = DIV.find('div', attrs={'class': 'ads-visurl'}).text
@@ -295,30 +267,33 @@ def divs_text_shelves(my_divs, url_counter, url_ques, my_divs_requests=None):
             if contact.isdigit() and len(contact) == 11:  # если осталось 11 цифр то это ном.телефона
                 my_company_contact = my_company_contact[len(my_company_contact) - len('0 (000) 000-00-00'):]
             else:
-                my_company_contact = ''
+                my_company_contact = 'N\A'
             log_visrec('company_contact ', my_company_contact)
-        except:
+        except Exception as ce:
+            log_visrec('company_contact', str(ce.args), True)
             my_company_contact: str = ''
         try:
             my_company_title: str = DIV.text.replace('Почему мне показано это объявление?', ' ')
             my_company_title.replace('Реклама', '').strip()
             my_company_title.replace(my_company_contact, ' ')
             log_visrec('company_title ', my_company_title)
-        except:
+        except Exception as ce:
+            log_visrec('my_company_title', str(ce.args), True)
             my_company_title: str = 'N\A'
         log_visrec('конец итерации', f' * * * \n')
 
+        # если требуется количество ответов на каждый url - ставим url counter
         my_divs_requests.append({'rowNom': i_row,
                                  'ques': url_ques,
                                  'company_title': my_company_title,
                                  'company_cid': my_company_cid,
                                  'company_link_1': my_company_link_1,
-                                 'company_sitelinks': my_company_sitelinks,
+                                 'company_site_links': my_company_site_links,
                                  'company_text': my_company_text,
                                  'company_contact': my_company_contact})
         i_row = i_row + 1
 
-    log_visrec('divs_text_shelves_divs_requests', my_divs_requests, False, True)
+    log_visrec('divs_text_shelves(divs_requests)', my_divs_requests)
     return my_divs_requests
 
 
@@ -349,11 +324,12 @@ def file_writer(my_divs_requests, my_full_path):
                         wb.Worksheets('Лист1').Cells(doc_row, 1).Value = divs_iter['rowNom']
                     else:
                         wb.Worksheets('Лист1').Cells(doc_row, 1).Value = doc_row - 1
+
                     wb.Worksheets('Лист1').Cells(doc_row, 2).Value = divs_iter['ques']
                     wb.Worksheets('Лист1').Cells(doc_row, 3).Value = divs_iter['company_title']
                     wb.Worksheets('Лист1').Cells(doc_row, 4).Value = divs_iter['company_cid']
                     wb.Worksheets('Лист1').Cells(doc_row, 5).Value = divs_iter['company_link_1']
-                    wb.Worksheets('Лист1').Cells(doc_row, 6).Value = divs_iter['company_sitelinks']
+                    wb.Worksheets('Лист1').Cells(doc_row, 6).Value = divs_iter['company_site_links']
                     wb.Worksheets('Лист1').Cells(doc_row, 7).Value = divs_iter['company_text']
                     wb.Worksheets('Лист1').Cells(doc_row, 8).Value = divs_iter['company_contact']
                     doc_row += 1
@@ -361,14 +337,17 @@ def file_writer(my_divs_requests, my_full_path):
                 com_client.Dispatch("Excel.Application").DisplayAlerts = False  # отключаем обновление экрана
                 wb.Close(True, my_full_path)  # сохраняем изменения и закрываем
                 log_visrec('file_writer', 'Данные записаны', True)
-            except:
+            except Exception as ce:
                 log_visrec('file_writer', 'Не удалось открыть книгу', True)
+                log_visrec('file_writer', str(ce.args), True)
                 ExcelApp.app_close()
-        except:
+        except Exception as ce:
             log_visrec('file_writer', 'Не удалось записать данные', True)
+            log_visrec('file_writer', str(ce.args), True)
             return
-    except:
+    except Exception as ce:
         log_visrec('file_writer', 'Ошибка при создании файла', True)
+        log_visrec('file_writer', str(ce.args), True)
     finally:
 
         ExcelApp.app_close()
@@ -377,7 +356,6 @@ def file_writer(my_divs_requests, my_full_path):
 @timeit
 def main():
     """Основная функция с параметрами"""
-    # идетификатор
 
     # базовый запрос
     base_url: str = f'https://www.google.com/search'
@@ -389,14 +367,18 @@ def main():
     url_max_pos = 2
     # Задаём регион
     region = '+' + 'Краснодар'
+    # колличество одновременных процессов / потоков
+    max_process = 2
 
-    divs_requests: list = []  # создаем список
+    urls = url_constructor(queries_path, base_url, region, url_max_pos)  # конструктор url
 
-    urls = url_constructor(queries_path, base_url, region, url_max_pos)
+    if max_process == 1:
+        divs_requests = Parser_GOO(urls)  # последовательные запросы
+        file_writer(divs_requests, full_path)  # запись данных в файл
+    else:
+        divs_requests = Parser_GOO_parallel(urls, max_process)  # паралельные запросы
+        file_writer(divs_requests, full_path)  # запись данных в файл
 
-    # divs_requests = Parser_YD_GOO(urls, divs_requests)  # последовательные запросы
-    divs_requests = Parser_YD_GOO_parallel(urls, divs_requests)  # паралельные запросы
-    file_writer(divs_requests, full_path)
     log_visrec('main', 'Парсинг завершен', True)
 
 
